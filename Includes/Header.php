7c39b078cd3fecfb7214743f3e3ad942d720ba48
endroni <rubao.rcs@gmail.com>
2021-01-22 16:51:39 -0300

    layout iniciado

---
+++ Includes/Header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>CRUD</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
+<body>
+
+	<div class="Nav">
+		<li><a href="cadastrar">Cadastrar</a></li>
+		<li><a href="selecionar">Buscar</a></li>
+		<li><a href="deletar">Apagar</a></li>
+		<li><a href="atualizar">Atualizar</a></li>
+	</div>
